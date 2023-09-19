ba5faf9b6f13f498f5f5ad15fb2fff0130fde0fd
CaptainVee <captainvee3@gmail.com>
2023-09-19 23:03:30 +0100

    fixed typo

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UsersListView
 
 urlpatterns = [
-    path('users//<int:org_id>/', UsersListView.as_view(), name='users-list'),
+    path('users/<int:org_id>/', UsersListView.as_view(), name='users-list'),
 ]
