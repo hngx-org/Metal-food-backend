a49ca1519f55b532a1090865b6e8260ee9fbed3e
Code bee <codebee345@outlook.com>
2023-09-19 17:54:04 +0100

    added withdrawals and lunch models

--- zeus/free_lunch/models.py
+++ zeus/free_lunch/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Organization(models.Model):
+    id = models.BigAutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=50)
+    lunch_price = models.DecimalField(max_digit=10, decimal_places=2)
+    currency = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
 class Users(models.Model):
     """
      user model
     """
     id = models.AutoField(primary_key=True, unique=True)
-    org_id = models.ForeignKey('Organization', on_delete=models.SET_NULL)
+    org_id = models.ForeignKey(Organization, on_delete=models.SET_NULL)
     name = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to='profile_image/')
     email = models.EmailField(unique=True)
@@ -23,11 +32,18 @@ class Users(models.Model):
     def __str__(self):
         return self.name
 
-class Organization(models.Model):
-    id = models.BigAutoField(primary_key=True, unique=True)
-    name = models.CharField(max_length=50)
-    lunch_price = models.DecimalField(max_digit=10, decimal_places=2)
-    currency = models.CharField(max_length=3)
 
-    def __str__(self):
-        return self.name
+class Lunches(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    sender_id = models.ForeignKey(Users, on_delete=models.CASCADE, null=False, related_name='lunch_sender')
+    reciever_id = models.ForeignKey(Users, on_delete=models.CASCADE, null=False, related_name='lunch_reciever')
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    note = models.TextField(null=True)
+
+class Withdrawals(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    status= models.CharField(max_length=30)
+    amount = models.DecimalField()
+    created_at = models.DateTimeField(auto_now=True)
