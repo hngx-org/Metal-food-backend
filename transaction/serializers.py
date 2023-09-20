6c06a1d62e3acb39fce575d2dd1f1f18fb611d05
Abednego Emonena <emonenaabednego@gmail.com>
2023-09-20 07:31:43 +0000

    created withdrawal request serializers

---
+++ transaction/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+class WithdrawalRequestSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+    bank_account = serializers.CharField(max_length=20)
+    bank_name = serializers.CharField(max_length=50)
+    account_name = serializers.CharField(max_length=255)
