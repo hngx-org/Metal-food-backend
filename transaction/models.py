dae58a3af4248dd87d6063bc8afdf29386a9f4f3
web-weebie <twinklet969@gmail.com>
2023-09-20 12:48:51 +0100

    Feature: Added Lunch history retrieval endpoint

--- transaction/models.py
+++ transaction/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 from users.models import Users
 
-# Create your models here.
 
-class Lunches(models.Model):
-    id = models.BigAutoField(primary_key=True)
+class Lunch(models.Model):
+    id = models.BigAutoField(primary_key=True, editable=False, null=False)
     sender_id = models.ForeignKey(Users, on_delete=models.CASCADE, null=False, related_name='lunch_sender')
     reciever_id = models.ForeignKey(Users, on_delete=models.CASCADE, null=False, related_name='lunch_reciever')
     quantity = models.IntegerField()
     redeemed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now=True)
-    note = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField()
 
     def __str__(self) -> str:
         return self.id
 
 class Withdrawals(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True, null=False, editable=False)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     status= models.CharField(max_length=30)
     amount = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.id
--- transaction/views.py
+++ transaction/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Lunches
+from .serializers import LunchSerializer
+
+
+class ListLunchHistory(generics.ListAPIView):
+    serializer_class = LunchSerializer
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [TokenAuthentication, BasicAuthentication, SessionAuthentication]
+
+
+    def get_queryset(self):
+        user = self.request.user 
+        query_set = Lunches.objects.filter(sender_id=user) | Lunches.objects.filter(receiver_id=user)
+        return query_set     
