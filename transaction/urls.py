8fab8aa67e80f33e85e7e2bf3c2d627e2f7d1b5a
Abednego Emonena <emonenaabednego@gmail.com>
2023-09-20 08:20:08 +0000

    added views import statement

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path 
+from django.urls import path
+from .views import WithdrawalRequestCreateView
 
 urlpatterns = [
    path('/api/withdrawal/request', WithdrawalRequestCreateView.as_view(), name="withdrawal_request")
