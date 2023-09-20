84a9a7548dae8d6c4283ceeeef28d47deefba7c7
Code bee <codebee345@outlook.com>
2023-09-20 06:35:30 +0100

    updated models

--- zeus/free_lunch/models.py
+++ zeus/free_lunch/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Organization(models.Model):
     id = models.BigAutoField(primary_key=True, unique=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     lunch_price = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3)
 
@@ -17,19 +17,22 @@ class Users(models.Model):
     """
     id = models.AutoField(primary_key=True, unique=True)
     org_id = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
-    first_name = models.CharField(max_length=100, null=False)
-    last_name = models.CharField(max_length=100, null=False)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
     profile_picture = models.ImageField(upload_to='profile_image/')
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
-    password_hash = models.CharField(max_length=100)
-    refresh_token = models.CharField(max_length=100)
+    password_hash = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
     bank_number = models.CharField(max_length=20)
     bank_code = models.CharField(max_length=30) 
     bank_name = models.CharField(max_length=30)
+    bank_region = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     isAdmin = models.BooleanField(default=False)
     lunch_credit_balance = models.IntegerField()
+    currency = models.CharField(max_length=255)
+    currency_code = models.CharField(max_length=10)
     updated_at = models.DateTimeField()
 
     
