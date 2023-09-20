20f647fef4e314abe7944ff9de872243dfefb10d
Decodeine <abionaadesola@yahoo.com>
2023-09-20 12:40:53 +0100

    updated model message

--- users/models.py
+++ users/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+
+
 class Organization(models.Model):
     id = models.BigAutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=50)
@@ -8,31 +11,68 @@ class Organization(models.Model):
     def __str__(self):
         return self.name
 
-class Users(models.Model):
-    """
-     user model
-    """
-    id = models.AutoField(primary_key=True, unique=True)
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', False)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **kwargs)
+
+class Users(AbstractBaseUser, PermissionsMixin):
     org_id = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
-    first_name = models.CharField(max_length=100, null=False)
-    last_name = models.CharField(max_length=100, null=False)
-    profile_picture = models.ImageField(upload_to='profile_image/')
+    id = models.ForeignKey(Organization, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to='profile_image/', null=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
-    password_hash = models.CharField(max_length=100)
-    refresh_token = models.CharField(max_length=100)
-    bank_number = models.CharField(max_length=20)
-    bank_code = models.CharField(max_length=30) 
-    bank_name = models.CharField(max_length=30)
+    refresh_token = models.CharField(max_length=255, null=True)
+    bank_number = models.CharField(max_length=20, null=True)
+    bank_code = models.CharField(max_length=30, null=True)
+    bank_name = models.CharField(max_length=30, null=True)
+    bank_region = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    isAdmin = models.BooleanField(default=False)
-    lunch_credit_balance = models.IntegerField()
-    updated_at = models.DateTimeField()
+    lunch_credit_balance = models.DecimalField()
+    currency = models.CharField(max_length=255)
+    currency_code = models.CharField(max_length=10)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
+
+    class Meta:
+        unique_together= ['id','email']
     
-    def __str__(self):
-        return self.name
 
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    def has_perm(self, perm, obj=None):
+        
+        return True
+    def has_module_perms(self, app_label):
+        
+        return True 
+    
 class OrganizationInvites(models.Model):
     id = models.ForeignKey(Organization, on_delete=models.CASCADE, primary_key=True)
     email = models.EmailField(unique=True)
