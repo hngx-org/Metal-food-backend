6d161aabe157fddfe9ab81f58d60645e2ccb0e32
Abednego Emonena <emonenaabednego@gmail.com>
2023-09-20 07:59:57 +0000

    added bank code to the serializers

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -4,4 +4,4 @@ class WithdrawalRequestSerializer(serializers.Serializer):
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
     bank_account = serializers.CharField(max_length=20)
     bank_name = serializers.CharField(max_length=50)
-    account_name = serializers.CharField(max_length=255)
+    bank_code = serializers.CharField(max_length=30)
