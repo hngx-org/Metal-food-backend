e41b4b4ef1b6f8ce8e40b92fc794eee83c45df81
Beloved-vince <vinceoludare@gmail.com>
2023-09-20 14:02:59 +0100

    fix custom models user app

--- zeus/settings.py
+++ zeus/settings.py
@@ -122,6 +122,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.Users'
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
@@ -171,3 +173,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
