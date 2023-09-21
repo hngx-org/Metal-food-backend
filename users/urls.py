890d8dc6226a14541f9624337635827e781534eb
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-22 00:20:43 +0100

    Implement UpdateLunchBalance endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
+from .views import UpdateOrganizationLunchWallet
 
 urlpatterns = [
-    
+    path('api/organization/<int:org_id>/wallet/update/', UpdateOrganizationLunchWallet.as_view()),
 ]
