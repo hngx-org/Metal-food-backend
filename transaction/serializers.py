54e62f29203c88ff68ead3ddac16340513d428fa
BrodaOJ56 <olujas1@gmail.com>
2023-09-21 13:11:21 +0100

    fixed-lunch-detail

---
+++ transaction/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Lunch, Users
+
+
+class LunchSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Lunch
+        fields = '__all__'
+
+class LunchDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lunch
+        fields = '__all__'
+
+class SendLunchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lunch
+        fields = ['reciever_id', 'quantity', 'note']
+
+class WithdrawalRequestSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+    bank_account = serializers.CharField(max_length=20)
+    bank_name = serializers.CharField(max_length=50)
+    bank_code = serializers.CharField(max_length=30)
+
