33c0e2f6878b33262171eaaa88bf614763710a41
Abednego Emonena <emonenaabednego@gmail.com>
2023-09-21 20:37:15 +0000

    crreated serializers for withdrawal request and withdrawal count

---
+++ transaction/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+
+class WithdrawalRequestSerializer(serializers.Serializer): 
+     amount = serializers.DecimalField(max_digits=10, decimal_places=2) 
+     bank_account = serializers.CharField(max_length=20) 
+     bank_name = serializers.CharField(max_length=50) 
+     bank_code = serializers.CharField(max_length=30) 
+  
+  
+  
+ class WithdrawalCountSerializer(serializers.Serializer): 
+     user_id = serializers.IntegerField() 
+     withdrawal_count =serializers.IntegerField() 
+ 
