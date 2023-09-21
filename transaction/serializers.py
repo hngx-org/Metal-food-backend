ab700e866d32a69372af4e4ec5a3f0f234f2bfef
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-09-21 15:44:42 +0100

    merge with withdrawal branch

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -6,23 +6,6 @@ class WithdrawalRequestSerializer(serializers.Serializer):
     bank_number = serializers.CharField(max_length=20)
     bank_name = serializers.CharField(max_length=50)
     bank_code = serializers.CharField(max_length=30)
-    bank_account = serializers.CharField(max_length=20)
-    status=serializers.CharField(read_only = True)
-
-    class Meta:
-        model = Withdrawals
-        fields = ['pk','amount' ,'status','bank_name', 'bank_code', 'bank_account']
-
-    def create(self, validated_data):
-       
-        bank_name = validated_data.pop('bank_name')
-        bank_code = validated_data.pop('bank_code')
-        bank_account = validated_data.pop('bank_account')
-  
-        withdrawals = Withdrawals.objects.create(**validated_data)
-
-        return withdrawals
-
 
 class WithdrawalRequestGetSerializer(serializers.ModelSerializer):
     class Meta:
