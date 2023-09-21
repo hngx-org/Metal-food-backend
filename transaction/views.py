c13a4d7989e87cf5420da71d4f56a8adb95d6309
BrodaOJ56 <olujas1@gmail.com>
2023-09-22 00:17:41 +0100

    return the queryset directly

--- transaction/views.py
+++ transaction/views.py
@@ -4,33 +4,32 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Lunch, Withdrawals
-from .serializers import LunchSerializer, WithdrawalCountSerializer
+from .serializers import LunchSerializers#, WithdrawalCountSerializer
 
 
 class GetAllLunch(generics.ListAPIView):
-    serializer_class = LunchSerializer
-    permission_classes = [IsAuthenticated,]
+    serializer_class = LunchSerializers
+    permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication, BasicAuthentication, SessionAuthentication]
-
 
     def get_queryset(self):
         user = self.request.user 
         query_set = Lunch.objects.filter(sender_id=user) | Lunch.objects.filter(receiver_id=user)
-        return Response({'Lunch History': query_set}, status.HTTP_200_OK)  
+        return query_set 
 
 
         """
         Get the count of withdrawal made by user
         """
-class WithdrawalCountView(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
+# class WithdrawalCountView(generics.RetrieveAPIView):
+#     permission_classes = [IsAuthenticated]
 
-    def retrieve(self, request, *args, **kwargs):
-        #  count of withdrawal requests for the user
-        withdrawal_count = Withdrawals.objects.filter(user=request.user).count()
-        serializer = WithdrawalCountSerializer({
-            "user_id": request.user.id,
-            "withdrawal_count": withdrawal_count,
-        })
+#     def retrieve(self, request, *args, **kwargs):
+#         #  count of withdrawal requests for the user
+#         withdrawal_count = Withdrawals.objects.filter(user=request.user).count()
+#         serializer = WithdrawalCountSerializer({
+#             "user_id": request.user.id,
+#             "withdrawal_count": withdrawal_count,
+#         })
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
