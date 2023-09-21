cfc59467266ba89a7387abfca6199c780b8000fe
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-09-21 23:40:55 +0100

    Implement List Users and Search Users Endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-
+from . import views
 urlpatterns = [
-    
+    path('api/user/all', views.ListUsersView.as_view(), name='list-users'),
+    path('api/search/<str:nameoremail>/', views.SearchUserView.as_view(), name='search-user'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from .models import Users
+from .user_serializers import UserSerializer
 
 # Create your views here.
+
+
+class ListUsersView(generics.ListAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class SearchUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        nameoremail = self.kwargs.get('nameoremail')  # Get the nameoremail parameter from the URL
+        # Perform a case-insensitive search on first_name, last_name, and email
+        queryset = Users.objects.filter(
+            Q(first_name__icontains=nameoremail) |
+            Q(last_name__icontains=nameoremail) |
+            Q(email__icontains=nameoremail)
+        )
+        return queryset
+
+    def retrieve(self, request, nameoremail):
+        queryset = self.get_queryset()
+        user = get_object_or_404(queryset)
+        serializer = UserSerializer(user)
+        return Response({
+            "message": "User found",
+            "statusCode": status.HTTP_200_OK,
+            "data": serializer.data
+        })
