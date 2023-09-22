549b51b16a47d47e90437a175d9bda0585bd5492
Abednego Emonena <76168861+codynego@users.noreply.github.com>
2023-09-22 05:12:49 -0700

    Update serializers.py

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -10,7 +10,7 @@ class LunchSerializers(serializers.ModelSerializer):
 
 class WithdrawalRequestSerializer(serializers.Serializer):
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    bank_account = serializers.CharField(max_length=20)
+    bank_number = serializers.CharField(max_length=20)
     bank_name = serializers.CharField(max_length=50)
     bank_code = serializers.CharField(max_length=30)
 
