4107310970206ad0eab118d11ad5472a9fd17ded
Abednego Emonena <emonenaabednego@gmail.com>
2023-09-22 11:32:01 +0000

    fixed an error, changed bank_account to bank_number

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -9,7 +9,7 @@ class LunchSerializers(serializers.ModelSerializer):
         fields = '__all__'
 class WithdrawalRequestSerializer(serializers.Serializer):
     amount=serializers.DecimalField(max_digits=10,decimal_places=2)
-    bank_account=serializers.CharField(max_length=20)
+    bank_number=serializers.CharField(max_length=20)
     bank_name=serializers.CharField(max_length=50)
     bank_code=serializers.CharField(max_length=30)
 
