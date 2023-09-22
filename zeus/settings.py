9da2d1fa9a15c61868f058ffa310c738e30151fe
Ohunayo Gege <ohunayogege@gmail.com>
2023-09-22 18:28:18 +0100

    added the URL for static

--- zeus/settings.py
+++ zeus/settings.py
@@ -212,6 +212,7 @@ EMAIL_USE_SSL = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
